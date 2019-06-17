dd430fac449175dd0083405ef9330865b64c4f1f
Igor Krupin <ikrupin@innovativeautomation.com>
2019-06-17 11:49:57 -0400

    downgraded to 2.1 as it has LTS support

--- src/api.cribhub.ifttt/Program.cs
+++ src/api.cribhub.ifttt/Program.cs
@@ -1,8 +1,4 @@
-﻿using Amazon.Lambda.APIGatewayEvents;
-using Amazon.Lambda.Core;
-using Amazon.Lambda.RuntimeSupport;
-using Amazon.Lambda.Serialization.Json;
-using Microsoft.AspNetCore;
+﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using System;
 using System.Threading.Tasks;
@@ -11,36 +7,11 @@ namespace api.cribhub.ifttt
 {
     public class Program
     {
-        public static async Task Main(string[] args)
-        {
-            if (string.IsNullOrEmpty(Environment.GetEnvironmentVariable("AWS_LAMBDA_FUNCTION_NAME")))
-            {
-                RunLocally(args);
-            }
-            else
-            {
-                await RunLambda();
-            }
-        }
-
-        static async Task RunLambda()
-        {
-            var lambdaEntry = new LambdaEntryPoint();
-
-            using (var handlerWrapper = HandlerWrapper.GetHandlerWrapper<APIGatewayProxyRequest, APIGatewayProxyResponse>(lambdaEntry.FunctionHandlerAsync, new JsonSerializer()))
-            {
-                using (var bootstrap = new LambdaBootstrap(handlerWrapper))
-                {
-                    await bootstrap.RunAsync();
-                }
-            }
-        }
-
-        static void RunLocally(string[] args)
+        public static void Main(string[] args)
         {
             var builder = CreateWebHostBuilder(args);
 
-            builder.ConfigureKestrel(o => o.AddServerHeader = false); // do not adverstise Kestrel
+            builder.UseKestrel(o => o.AddServerHeader = false); // do not adverstise Kestrel
 
             var webHost = builder.Build();
 
--- src/api.cribhub.ifttt/Startup.cs
+++ src/api.cribhub.ifttt/Startup.cs
@@ -26,7 +26,7 @@ namespace api.cribhub.ifttt
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
             services.AddApiVersioning(o =>
             {
@@ -39,9 +39,6 @@ namespace api.cribhub.ifttt
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-
-            app.UseStaticFiles();
-
             app.UseMvc(ConfigureRoutes);
         }
 

7f425b5810457a347bb58a5562bda7970a7e1c53
Igor Krupin <ikrupin@innovativeautomation.com>
2019-06-18 13:40:25 -0400

    added logging

--- src/api.cribhub.ifttt/Filters/ServiceKeyCheckAttribute.cs
+++ src/api.cribhub.ifttt/Filters/ServiceKeyCheckAttribute.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
+using NLog;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,12 +11,17 @@ namespace api.cribhub.ifttt.Filters
     public class ServiceKeyCheckAttribute : ActionFilterAttribute
     {
         static readonly string _iftttSvcKeyEnvVarName = "IFTTT_SERVICE_KEY";
+        static readonly string _iftttSvcHeaderName = "IFTTT-Service-Key";
         static readonly string _serviceKey = Environment.GetEnvironmentVariable(_iftttSvcKeyEnvVarName);
+
+        static readonly Logger logger = LogManager.GetCurrentClassLogger();
 
         public override void OnActionExecuting(ActionExecutingContext context)
         {
             if(string.IsNullOrEmpty(_serviceKey))
             {
+                logger.Error($"{_iftttSvcKeyEnvVarName} is not set");
+
                 throw new Exception($"service key cannot be found. Ensure {_iftttSvcKeyEnvVarName} is set");
             }
 
@@ -28,14 +34,18 @@ namespace api.cribhub.ifttt.Filters
                 return;
             }
 
-            string svcKey = req.Headers["IFTTT-Service-Key"];
+            string svcKey = req.Headers[_iftttSvcHeaderName];
 
             if(string.IsNullOrEmpty(svcKey))
             {
+                logger.Error($"{_iftttSvcHeaderName} was not provided");
+
                 context.Result = new StatusCodeResult(401);
             }
             else if(!svcKey.Equals(_serviceKey, StringComparison.OrdinalIgnoreCase))
             {
+                logger.Error($"the provided {_iftttSvcHeaderName} value of {svcKey} does not match the key in the configuration");
+
                 context.Result = new StatusCodeResult(401);
             }
         }

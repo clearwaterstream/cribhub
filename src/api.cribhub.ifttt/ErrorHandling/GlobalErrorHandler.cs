3973ea29e322043619d7a68da12b1c86a9192fa2
Igor Krupin <clearwaterstream@gmail.com>
2019-06-18 19:04:56 -0400

    added cw logging

---
+++ src/api.cribhub.ifttt/ErrorHandling/GlobalErrorHandler.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Http;
+using NLog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace api.cribhub.ifttt.ErrorHandling
+{
+    public static class GlobalErrorHandler
+    {
+        static readonly Logger logger = LogManager.GetCurrentClassLogger();
+
+        public static async Task ExceptionHandlerDelegate(HttpContext context)
+        {
+            var exceptionHandler = context.Features.Get<IExceptionHandlerFeature>();
+
+            var resp = context.Response;
+
+            if (exceptionHandler == null)
+            {
+                logger.Error("IExceptionHandlerFeature is not available");
+
+                await WriteErrorMessage(resp, null);
+
+                return;
+            }
+
+            var ex = exceptionHandler.Error;
+
+            try
+            {
+                _onError?.Invoke(context, ex);
+            }
+            catch { /* do nothing */ }
+
+            if (ex == null)
+            {
+                logger.Error("unknown exception caught in the global handler");
+
+                await WriteErrorMessage(resp, null);
+
+                return;
+            }
+
+            logger.Error(ex, "caught in global error handler");
+
+            await WriteErrorMessage(resp, null);
+        }
+
+        static Task WriteErrorMessage(HttpResponse response, string errorMsg)
+        {
+            response.StatusCode = 500;
+
+            if (string.IsNullOrEmpty(errorMsg))
+                errorMsg = "Appolgies, something went wrong. The error has been logged.";
+
+            return response.WriteAsync(errorMsg, CancellationToken.None);
+        }
+
+        static Action<HttpContext, Exception> _onError;
+        static readonly object _onErrorLock = new object();
+
+        public static event Action<HttpContext, Exception> OnError
+        {
+            add
+            {
+                lock (_onErrorLock)
+                {
+                    _onError += value;
+                }
+            }
+            remove
+            {
+                lock (_onErrorLock)
+                {
+                    _onError -= value;
+                }
+            }
+        }
+    }
+}
--- src/api.cribhub.ifttt/Startup.cs
+++ src/api.cribhub.ifttt/Startup.cs
@@ -2,12 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using api.cribhub.ifttt.ErrorHandling;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Routing;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using NLog;
 
 namespace api.cribhub.ifttt
 {
@@ -16,10 +18,17 @@ namespace api.cribhub.ifttt
         public IConfiguration Configuration { get; }
         public IHostingEnvironment HostingEnvironment { get; }
 
+        readonly Logger logger = null;
+
         public Startup(IConfiguration configuration, IHostingEnvironment env)
         {
             Configuration = configuration;
             HostingEnvironment = env;
+
+            var logFactory = LogManager.LoadConfiguration("NLog.config");
+            logger = logFactory.GetCurrentClassLogger();
+
+            logger.Info("app is booting");
         }
 
         // This method gets called by the runtime. Use this method to add services to the container.
@@ -39,12 +48,25 @@ namespace api.cribhub.ifttt
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            var appLifetime = app.ApplicationServices.GetRequiredService<IApplicationLifetime>();
+
+            appLifetime.ApplicationStopping.Register(OnShuttingDown);
+
+            app.UseExceptionHandler(new ExceptionHandlerOptions() { ExceptionHandler = GlobalErrorHandler.ExceptionHandlerDelegate });
+
             app.UseMvc(ConfigureRoutes);
         }
 
         protected virtual void ConfigureRoutes(IRouteBuilder routes)
         {
             routes.MapRoute("versioned", "v{version:apiVersion}/{controller=Home}/{action=Index}/{id?}");
+        }
+
+        void OnShuttingDown()
+        {
+            logger.Info("app is shutting down");
+
+            LogManager.Flush(3000); // flush any remaining messages
         }
     }
 }

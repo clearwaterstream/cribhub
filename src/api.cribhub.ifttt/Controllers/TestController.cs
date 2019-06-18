3e0ed6c7d683cf127481d5b006e61307dc78adb5
Igor Krupin <ikrupin@innovativeautomation.com>
2019-06-18 13:21:13 -0400

    added nlog

---
+++ src/api.cribhub.ifttt/Controllers/TestController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace api.cribhub.ifttt.Controllers
+{
+    public class TestController : ControllerBase
+    {
+        [HttpPost]
+        public IAsyncResult Setup()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/api.cribhub.ifttt/Filters/ServiceKeyCheckAttribute.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace api.cribhub.ifttt.Filters
+{
+    public class ServiceKeyCheckAttribute : ActionFilterAttribute
+    {
+        static readonly string _iftttSvcKeyEnvVarName = "IFTTT_SERVICE_KEY";
+        static readonly string _serviceKey = Environment.GetEnvironmentVariable(_iftttSvcKeyEnvVarName);
+
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            if(string.IsNullOrEmpty(_serviceKey))
+            {
+                throw new Exception($"service key cannot be found. Ensure {_iftttSvcKeyEnvVarName} is set");
+            }
+
+            var req = context?.HttpContext?.Request;
+
+            if (req == null)
+            {
+                base.OnActionExecuting(context);
+
+                return;
+            }
+
+            string svcKey = req.Headers["IFTTT-Service-Key"];
+
+            if(string.IsNullOrEmpty(svcKey))
+            {
+                context.Result = new StatusCodeResult(401);
+            }
+            else if(!svcKey.Equals(_serviceKey, StringComparison.OrdinalIgnoreCase))
+            {
+                context.Result = new StatusCodeResult(401);
+            }
+        }
+    }
+}
